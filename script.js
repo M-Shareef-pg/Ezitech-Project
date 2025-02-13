91d2a94df63cdefe4d59ad4ab8b0542ea42caf32
M-Shareef-pg <najmussaqibpro@gmail.com>
2025-02-13 16:30:18 +0500

    message

---
+++ script.js
@@ -0,0 +1,30 @@
+let anchors = document.querySelectorAll(".anchor");
+let items = ["one", "two", "three", "four", "five", "six"];
+
+document.querySelector("#one").style.color = "black";
+
+anchors.forEach(anchor => {
+    anchor.addEventListener("mouseover", () => {
+        items.forEach(item => {
+            if (item === anchor.id) {
+                document.querySelector(`#${item}`).style.color = "black";
+            } else {
+                document.querySelector(`#${item}`).style.color = "gray";
+            }
+        });
+    });
+
+    anchor.addEventListener("mouseout", () => {
+        items.forEach(item => {
+            if (item === "one") {
+                document.querySelector(`#${item}`).style.color = "black";
+            } else {
+                document.querySelector(`#${item}`).style.color = "initial";
+            }
+        });
+    });
+});
+
+
+
+
